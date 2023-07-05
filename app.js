0a480844e45ab61089edd954d34a14e5a365902a
Akshit Modhiya <akshitmodhiya@gmail.com>
2023-07-05 14:56:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const tagsElem = document.getElementById('tags');
+const textArea = document.getElementById('textarea');
+
+textArea.focus();
+
+textArea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    });
+
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+
+  tagsElem.innerHTML = '';
+
+  tags.forEach(tag => {
+    const tagElem = document.createElement('span');
+    tagElem.classList.add('tag');
+    tagElem.innerText = tag;
+    tagsElem.appendChild(tagElem);
+  });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highLightTag(randomTag);
+
+    setTimeout(() => {
+      unHighLightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highLightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highLightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unHighLightTag(tag) {
+  tag.classList.remove('highlight');
+}
